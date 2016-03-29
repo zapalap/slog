480b13fe6f4aea1f9b243d596f21fb04ddf299db
Przemyslaw Zapala <przemyslaw.zapala@capgemini.com>
2016-03-29 15:50:51 +0200

    Improve handling of log file watching

--- server/socket-server.js
+++ server/socket-server.js
@@ -1,8 +1,9 @@
 'use strict';
 const
     Promise = require('bluebird'),
-    fs = require('fs'),
-    lineReader = require('line-by-line-promise'),
+    chokidar = require('chokidar'),
+    lineReaderPromise = require('line-by-line-promise'),
+    lineReaderAsync = require('line-by-line'),
     api = require('./http_api/http_api.js')(3001),
     sock = require('./sockets/server.js')(3000),
     filename = process.argv[2];
@@ -12,7 +13,7 @@ const
      api.get('/api/entries', function(req, res) {
         let entries = [],
             lines = [],
-            lr = new lineReader(filename);
+            lr = new lineReaderPromise(filename);
       
       Promise.coroutine(function* (){
           var line;
@@ -37,11 +38,18 @@ const
        
     });
     
-    let watcher = fs.watch(filename, function() {
-        let entry = {id:id++, timestamp:Date.now(), text:"File " + filename + " has changed."};
-        sock.emit('message', {data:entry});
-        console.log('Just sent '+ JSON.stringify(entry));
-    })
+    let watcher = chokidar.watch(filename).on('change', function() {
+        let lr = new lineReaderAsync(filename);
+        let lastLine = "";
+        
+        lr.on('line', function(line) {
+            lastLine = line;
+        }).on('end', function() {
+            let entry = {id:id++, timestamp:Date.now(), text:lastLine};
+            sock.emit('message', {data:entry});
+            console.log('Just sent '+ JSON.stringify(entry));
+        });
+    });
     
     
     
