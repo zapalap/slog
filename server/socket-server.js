7576f065e6fd3e14dda6d849b4b83bb2a859c5ea
zapalap <przemyslaw.zapala@gmail.com>
2016-03-31 21:43:40 +0200

    Make server ignore empty lines and duplicate line numbers

--- server/socket-server.js
+++ server/socket-server.js
@@ -8,46 +8,57 @@ const
     sock = require('./sockets/server.js')(3000),
     filename = process.argv[2];
     
-     api.get('/api/entries', function(req, res) {
-        let entries = [],
-            lines = [],
-            lr = new lineReaderPromise(filename);
-      
-      Promise.coroutine(function* (){
-          var line;
-          var id = 0;
-          
-          while((line = yield lr.readLine()) !== null) {
-               entries.push(
-            {
-                id:++id,
+var lastSentLineIndex = 0;
+
+api.get('/api/entries', function(req, res) {
+    let entries = [],
+        lines = [],
+        lr = new lineReaderPromise(filename);
+
+    Promise.coroutine(function*() {
+        var line;
+        var id = 0;
+
+        while ((line = yield lr.readLine()) !== null) {
+            entries.push({
+                id: ++id,
                 verboseMessage: line,
-                shortMessage:line,
+                shortMessage: line,
                 showing: true,
-                timestamp:Date.now(),
-                isMarked:false,
-                isDimmed:false,
-                isVisible:false
+                timestamp: Date.now(),
+                isMarked: false,
+                isDimmed: false,
+                isVisible: false
             });
-          }
-           res.send({entries:entries});
-      })();
-        
-       
-    });
-    
-    let watcher = chokidar.watch(filename).on('change', function() {
-        let lr = new lineReaderAsync(filename);
-        let lastLine = "";
-        
-        lr.on('line', function(line) {
-            lastLine = line;
-        }).on('end', function() {
-            let entry = {timestamp:Date.now(), text:lastLine};
-            sock.emit('message', {data:entry});
-            console.log('Just sent '+ JSON.stringify(entry));
+        }
+        res.send({
+            entries: entries
         });
+    })();
+
+
+});
+
+let watcher = chokidar.watch(filename).on('change', function() {
+    let lr = new lineReaderAsync(filename);
+    let lastLine = "";
+    let lineIndex = 0;
+
+    lr.on('line', function(line) {
+        lastLine = line;
+        lineIndex++;
+    }).on('end', function() {
+        console.log(`[lineIndex:${lineIndex}, lastSentLineIndex:${lastSentLineIndex}, willSend:${lastLine.trim() !== "" && !(lineIndex === lastSentLineIndex)}]`);
+        if (lastLine.trim() !== "" && !(lineIndex === lastSentLineIndex)) {
+            let entry = {
+                timestamp: Date.now(),
+                text: lastLine
+            };
+            sock.emit('message', {
+                data: entry
+            });
+            lastSentLineIndex = lineIndex;
+            console.log(`Just sent` + JSON.stringify(entry));
+        }
     });
-    
-    
-    
+});
--- ui/app/components/log-hud.js
+++ ui/app/components/log-hud.js
@@ -1,60 +1,60 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  socketService: Ember.inject.service('socket-io'),
-  store: Ember.inject.service(),
-  didInsertElement() {
-    var self = this;
-    const socket = this.get('socketService').socketFor('http://localhost:3000');
-    let store = this.get('store');
-    socket.on('message', function(event) {
-      var newEntry = store.createRecord('entry', {
-          verboseMessage:event.data.text,
-          shortMessage:event.data.text,
-          showing:true,
-          isMarked:false,
-          isVisible:true,
-          timestamp:new Date(event.data.timestamp*1000).toString()
-      });
-    })
-  },
-  menuShouldShow: Ember.computed.or('atLeastOneMarked', 'filterIsNotEmpty'),
-  atLeastOneMarked: false,
-  filterIsNotEmpty: Ember.computed.notEmpty('filterValue'),
-  filterValue:'',
-  filteredLogs: Ember.computed('filterValue', 'logEntryList.@each', function() {
-    let filterValue = this.get('filterValue');
-    return this.get('logEntryList').filter(function(entry) {
-      return entry.get('verboseMessage').includes(filterValue);
-    });
-  }),
-  actions: {
-    lineMarked(logEntry) {
-      this.set('atLeastOneMarked', true);
+    socketService: Ember.inject.service('socket-io'),
+    store: Ember.inject.service(),
+    didInsertElement() {
+        var self = this;
+        const socket = this.get('socketService').socketFor('http://localhost:3000');
+        let store = this.get('store');
+        socket.on('message', function(event) {
+            var newEntry = store.createRecord('entry', {
+                verboseMessage: event.data.text,
+                shortMessage: event.data.text,
+                showing: true,
+                isMarked: false,
+                isVisible: true,
+                timestamp: new Date(event.data.timestamp * 1000).toString()
+            });
+        })
     },
-    lineUnmarked(logEntry) {
-      var marked = this.get('logEntryList').isAny('isMarked', true);
-      if (!marked) {
-        this.get('logEntryList').forEach(function(model) {
-          model.set('isDimmed', false);
+    menuShouldShow: Ember.computed.or('atLeastOneMarked', 'filterIsNotEmpty'),
+    atLeastOneMarked: false,
+    filterIsNotEmpty: Ember.computed.notEmpty('filterValue'),
+    filterValue: '',
+    filteredLogs: Ember.computed('filterValue', 'logEntryList.@each', function() {
+        let filterValue = this.get('filterValue');
+        return this.get('logEntryList').filter(function(entry) {
+            return entry.get('verboseMessage').includes(filterValue);
         });
-        this.set('atLeastOneMarked', false);
-      }
+    }),
+    actions: {
+        lineMarked(logEntry) {
+            this.set('atLeastOneMarked', true);
+        },
+        lineUnmarked(logEntry) {
+            var marked = this.get('logEntryList').isAny('isMarked', true);
+            if (!marked) {
+                this.get('logEntryList').forEach(function(model) {
+                    model.set('isDimmed', false);
+                });
+                this.set('atLeastOneMarked', false);
+            }
+        },
+        resetAllLogLines() {
+            this.get('logEntryList').forEach(function(model) {
+                model.set('isMarked', false);
+                model.set('isDimmed', false);
+            });
+            this.set('filterValue', '');
+            this.set('atLeastOneMarked', false);
+        },
+        dimAllNotSelectedLogLines() {
+            var notSelected = this.get('logEntryList').filterBy('isMarked', false);
+            notSelected.forEach(function(model) {
+                console.log(model);
+                model.set('isDimmed', !model.get('isDimmed'));
+            });
+        }
     },
-    resetAllLogLines() {
-      this.get('logEntryList').forEach(function(model) {
-        model.set('isMarked', false);
-        model.set('isDimmed', false);
-      });
-      this.set('filterValue', '');
-      this.set('atLeastOneMarked', false);
-    },
-    dimAllNotSelectedLogLines() {
-      var notSelected = this.get('logEntryList').filterBy('isMarked', false);
-      notSelected.forEach(function(model) {
-        console.log(model);
-        model.set('isDimmed', !model.get('isDimmed'));
-      });
-    }
-  },
 });
